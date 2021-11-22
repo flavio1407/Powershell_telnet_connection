7451842242955cf38379ca0e3dc04606b8567c22
Flávio Azevedo <42978944+flavio1407@users.noreply.github.com>
2021-11-22 10:38:01 -0300

    Add files via upload

---
+++ telnet_lista_csv_v2.ps1
@@ -0,0 +1,30 @@
+
+#########Set ExecutionPolicy Unrestricted
+$lista_ip = import-csv lista_ip.csv
+$OutArray = @()
+Import-Csv lista_ip.csv |`
+ForEach-Object { 
+    try {
+        $remote = $_.remoteHost
+        $porta = $_.port
+        $socket = new-object System.Net.Sockets.TcpClient($remote, $porta)
+    } catch [Exception] {
+        $objt = "" | Select "remoteHost", "port", "status"
+        $objt.remoteHost = $remote
+        $objt.port = $porta
+        $objt.status = "failed"
+        Write-Host $objt
+        $outarray += $objt
+        $objt = $null
+        return
+    }
+    $objt = "" | Select "remoteHost", "port", "status"
+    $objt.remoteHost = $remote
+    $objt.port = $porta
+    $objt.status = "success"
+    Write-Host $objt
+    $outarray += $objt
+    $objt = $null
+    return
+}
+$outarray | export-csv -path "resultado.csv" -NoTypeInformation
